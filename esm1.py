97c182d70abf0020a66574dab4065d33227f474a
Frank Grijalva <Trey@Frank-Grijalva.local>
2024-04-29 12:48:00 +0200

    updated the script with plots and started on the next models

--- esm1.py
+++ esm1.py
@@ -10,33 +10,54 @@ dt = 300.0 #s?
 fCor = 0.00011 #Hz
 
 #data struct setup
+##time step array and analytical solution array
 timeArr = np.zeros(numTimeSteps)
 uAnaArr = np.zeros(numTimeSteps)
-uArr = np.zeros(numTimeSteps)
-vArr = np.zeros(numTimeSteps)
+
+##arrays for euler fwd method
+uEulerArr = np.zeros(numTimeSteps)
+vEulerArr = np.zeros(numTimeSteps)
 eulerErrArr = np.zeros(numTimeSteps)
 
-#initial conditions
-##sim init cond
-uArr[0] = 10.0
+##arrays for the Heun method
+uHeunArr = np.zeros(numTimeSteps)
+vHeunArr = np.zeros(numTimeSteps)
+heunErrArr = np.zeros(numTimeSteps)
 
-#matching that for the analytical answer comparison
-uAnaArr[0] = uArr[0]
+##arrays for the Matsuno method
+uMatsunoArr = np.zeros(numTimeSteps)
+vMatsunoArr = np.zeros(numTimeSteps)
+matsunoErrArr = np.zeros(numTimeSteps)
+
+
+
+#initial conditions
+##sim init cond ar all the same
+uEulerArr[0] = 10.0
+uHeunArr[0] = uEulerArr[0]
+uMatsunoArr[0] = uEulerArr[0]
+
+#matching analytical answer comparison
+uAnaArr[0] = uEulerArr[0]
 
 
 #mathing
-##euler forward scheme
-###loop over time
+##loop over time
 for nt in range(1,numTimeSteps):
+	#euler forward scheme
 	timeArr[nt] = timeArr[nt - 1] + dt
-	uArr[nt] = uArr[nt-1] + (fCor * vArr[nt-1] * dt)
-	vArr[nt] = vArr[nt-1] - (fCor * uArr[nt-1] * dt)
+	uEulerArr[nt] = uEulerArr[nt-1] + (fCor * vEulerArr[nt-1] * dt
+	vEulerArr[nt] = vEulerArr[nt-1] - (fCor * uEulerArr[nt-1] * dt)
 
 	#getting the analytical answer for this step
 	uAnaArr[nt] = uAnaArr[0] * np.cos(fCor * timeArr[nt])
 
 	#comparing the two to see the error
-	eulerErrArr[nt] = np.abs(uArr[nt] - uAnaArr[nt])
+	eulerErrArr[nt] = np.abs(uEulerArr[nt] - uAnaArr[nt])
+
+
+	#heun scheme
+	#!!!
 
 
 
@@ -49,16 +70,20 @@ plt.figure("Oscillation: Euler fwd vs analytical")
 plt.title("Oscillation: Euler fwd vs analytical")
 plt.xlabel("time\n(s)")
 plt.ylabel("u\n(m/s)")
-plt.plot(timeArr, uArr, '.b', label="Euler")
+plt.plot(timeArr, uEulerArr, '.b', label="Euler")
 plt.plot(timeArr, uAnaArr, "r", label="Analytical")
 plt.legend()
+
+##plot heun scheme
+
+##plot matsuno scheme
 
 #plotting the error (difference between the model and analytical solution)
 plt.figure("Error comparison")
 plt.title("error")
 plt.xlabel("time\n(s)")
 plt.ylabel("error (m/s)")
-plt.plot(timeArr, eulerErrArr, ".g")
+plt.plot(timeArr, eulerErrArr, ".g", label="Euler")
 
 ##Don't forget to show 'em if you got 'em
 plt.show()
