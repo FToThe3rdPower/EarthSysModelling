39672b091134dd2da74c55726ddcad6358bef028
Frank Grijalva <Trey@Frank-Grijalva.home>
2024-04-30 23:38:08 +0200

    updated the script with plots and started on the next models

--- esm1.py
+++ esm1.py
@@ -5,9 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #consts
-numTimeSteps = 720
-dt = 300.0 #s?
-fCor = 0.00011 #Hz
+numTimeSteps = 800
+dt = 200.0 #s?
+fCor = 0.001 #Hz
 
 #data struct setup
 ##time step array and analytical solution array
@@ -18,6 +18,11 @@ uAnaArr = np.zeros(numTimeSteps)
 uEulerArr = np.zeros(numTimeSteps)
 vEulerArr = np.zeros(numTimeSteps)
 eulerErrArr = np.zeros(numTimeSteps)
+
+##arrays for the leapfrog method
+uLeapFrogArr = np.zeros(numTimeSteps)
+vLeapFrogArr = np.zeros(numTimeSteps)
+leapFrogErrArr = np.zeros(numTimeSteps)
 
 ##arrays for the Heun method
 uHeunArr = np.zeros(numTimeSteps)
@@ -32,8 +37,9 @@ matsunoErrArr = np.zeros(numTimeSteps)
 
 
 #initial conditions
-##sim init cond ar all the same
+##sim init cond are all the same
 uEulerArr[0] = 10.0
+uLeapFrogArr[0] = uEulerArr[0]
 uHeunArr[0] = uEulerArr[0]
 uMatsunoArr[0] = uEulerArr[0]
 
@@ -46,19 +52,31 @@ uAnaArr[0] = uEulerArr[0]
 for nt in range(1,numTimeSteps):
 	#euler forward scheme
 	timeArr[nt] = timeArr[nt - 1] + dt
-	uEulerArr[nt] = uEulerArr[nt-1] + (fCor * vEulerArr[nt-1] * dt
+	uEulerArr[nt] = uEulerArr[nt-1] + (fCor * vEulerArr[nt-1] * dt)
 	vEulerArr[nt] = vEulerArr[nt-1] - (fCor * uEulerArr[nt-1] * dt)
+
+	#leapfrog scheme
+	##give it the second point from Euler
+	if nt == 1:
+		uLeapFrogArr[1] = uEulerArr[nt]
+		vLeapFrogArr[1] = vEulerArr[nt]
+
+	##proceed with the rest of the lf scheme
+	else:
+		uLeapFrogArr[nt] = fCor*vLeapFrogArr[nt-1]*2*dt + uLeapFrogArr[nt-2] 
+		vLeapFrogArr[nt] = -fCor*uLeapFrogArr[nt-1]*2*dt + vLeapFrogArr[nt-2]
+	
+	
+	#heun scheme
+	#!!!
+
 
 	#getting the analytical answer for this step
 	uAnaArr[nt] = uAnaArr[0] * np.cos(fCor * timeArr[nt])
 
-	#comparing the two to see the error
+	#comparing the schemes to see the error
 	eulerErrArr[nt] = np.abs(uEulerArr[nt] - uAnaArr[nt])
-
-
-	#heun scheme
-	#!!!
-
+	leapFrogErrArr[nt] = np.abs(uLeapFrogArr[nt] - uAnaArr[nt])
 
 
 #plotting
@@ -67,11 +85,21 @@ plt.close("all")
 
 ##plotting the estimated oscillation vs the actual
 plt.figure("Oscillation: Euler fwd vs analytical")
-plt.title("Oscillation: Euler fwd vs analytical")
+plt.title("Euler fwd vs analytical")
 plt.xlabel("time\n(s)")
 plt.ylabel("u\n(m/s)")
 plt.plot(timeArr, uEulerArr, '.b', label="Euler")
 plt.plot(timeArr, uAnaArr, "r", label="Analytical")
+# !!! put the other methods here too
+plt.legend()
+
+##plotting the central diff scheme
+plt.figure("Oscillation: center diff vs analytical")
+plt.title("Center diff vs analytical")
+plt.xlabel("time\n(s)")
+plt.ylabel("u\n(m/s)")
+plt.plot(timeArr, uLeapFrogArr, ".c", label="Leapfrog")
+plt.plot(timeArr, uAnaArr, "m", label="Analytical")
 plt.legend()
 
 ##plot heun scheme
@@ -84,10 +112,12 @@ plt.title("error")
 plt.xlabel("time\n(s)")
 plt.ylabel("error (m/s)")
 plt.plot(timeArr, eulerErrArr, ".g", label="Euler")
+plt.plot(timeArr, leapFrogErrArr, ".k", label="leapfrog")
+plt.legend()
+
 
 ##Don't forget to show 'em if you got 'em
 plt.show()
 
-##one for the comparison of model to the analytical solution
 ##one to show the error growth over time
 ##perhaps one for energy
