26d1e55da61fb90efd5bc6a07490013ed9168f6c
Frank Grijalva <Trey@Frank-Grijalva.local>
2024-05-01 12:48:17 +0200

    added energy plots, broken Matsuno scheme

--- esm1.py
+++ esm1.py
@@ -4,10 +4,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#consts
+#sim vars & consts
 numTimeSteps = 800
 dt = 200.0 #s?
 fCor = 0.001 #Hz
+initCond  = 10.0
 
 #data struct setup
 ##time step array and analytical solution array
@@ -18,33 +19,37 @@ uAnaArr = np.zeros(numTimeSteps)
 uEulerArr = np.zeros(numTimeSteps)
 vEulerArr = np.zeros(numTimeSteps)
 eulerErrArr = np.zeros(numTimeSteps)
+eulerEngArr = np.zeros(numTimeSteps)
 
 ##arrays for the leapfrog method
 uLeapFrogArr = np.zeros(numTimeSteps)
 vLeapFrogArr = np.zeros(numTimeSteps)
 leapFrogErrArr = np.zeros(numTimeSteps)
+leapFrogEngArr = np.zeros(numTimeSteps)
 
 ##arrays for the Heun method
 uHeunArr = np.zeros(numTimeSteps)
 vHeunArr = np.zeros(numTimeSteps)
 heunErrArr = np.zeros(numTimeSteps)
+heunEngArr = np.zeros(numTimeSteps)
+
 
 ##arrays for the Matsuno method
 uMatsunoArr = np.zeros(numTimeSteps)
 vMatsunoArr = np.zeros(numTimeSteps)
 matsunoErrArr = np.zeros(numTimeSteps)
-
+matsunoEngArr = np.zeros(numTimeSteps)
 
 
 #initial conditions
 ##sim init cond are all the same
-uEulerArr[0] = 10.0
-uLeapFrogArr[0] = uEulerArr[0]
-uHeunArr[0] = uEulerArr[0]
-uMatsunoArr[0] = uEulerArr[0]
+uEulerArr[0] = initCond
+uLeapFrogArr[0] = initCond
+uHeunArr[0] = initCond
+uMatsunoArr[0] = initCond
 
 #matching analytical answer comparison
-uAnaArr[0] = uEulerArr[0]
+uAnaArr[0] = initCond
 
 
 #mathing
@@ -67,24 +72,25 @@ for nt in range(1,numTimeSteps):
 		vLeapFrogArr[nt] = -fCor*uLeapFrogArr[nt-1]*2*dt + vLeapFrogArr[nt-2]
 	
 	#heun scheme
-	#
-	# Heun scheme
 	uHeunPred = uHeunArr[nt-1] + (fCor * vHeunArr[nt-1] * dt)
 	vHeunPred = vHeunArr[nt-1] - (fCor * uHeunArr[nt-1] * dt)
 	uHeunArr[nt] = uHeunArr[nt-1] + (fCor * (vHeunPred + vHeunArr[nt-1]) * 0.5 * dt)
 	vHeunArr[nt] = vHeunArr[nt-1] - (fCor * (uHeunPred + uHeunArr[nt-1]) * 0.5 * dt)
-	heunErrArr[nt] = np.abs(uHeunArr[nt] - uAnaArr[nt])
 
 	#matsuno
-	# Matsuno scheme
 	if nt == 1:
 	    vMatsunoArr[1] = vEulerArr[nt]
 	    print("Matsuno IC #2 trig")
 	else:
 	    uMatsunoArr[nt] = uMatsunoArr[nt-2] + 2 * (fCor * vMatsunoArr[nt-1] * dt)
 	    vMatsunoArr[nt] = vMatsunoArr[nt-1] - (fCor * (uMatsunoArr[nt-2] + uMatsunoArr[nt]) * dt)
-	matsunoErrArr[nt] = np.abs(uMatsunoArr[nt] - uAnaArr[nt])
+# !!! Add correction
 
+	#calculating energies
+	eulerEngArr[nt] = 0.5*(uEulerArr[nt]**2 + vEulerArr[nt]**2)
+	leapFrogEngArr[nt] = 0.5*(uLeapFrogArr[nt]**2 + vLeapFrogArr[nt]**2)
+	heunEngArr[nt] = 0.5*(uHeunArr[nt]**2 + vHeunArr[nt]**2)
+	matsunoEngArr[nt] = 0.5*(uMatsunoArr[nt]**2 + vMatsunoArr[nt]**2)
 
 	#getting the analytical answer for this step
 	uAnaArr[nt] = uAnaArr[0] * np.cos(fCor * timeArr[nt])
@@ -92,6 +98,8 @@ for nt in range(1,numTimeSteps):
 	#comparing the schemes to see the error
 	eulerErrArr[nt] = np.abs(uEulerArr[nt] - uAnaArr[nt])
 	leapFrogErrArr[nt] = np.abs(uLeapFrogArr[nt] - uAnaArr[nt])
+	heunErrArr[nt] = np.abs(uHeunArr[nt] - uAnaArr[nt])
+	matsunoErrArr[nt] = np.abs(uMatsunoArr[nt] - uAnaArr[nt])
 
 
 #plotting
@@ -100,46 +108,52 @@ plt.close("all")
 
 ##plotting the estimated oscillation vs the actual
 plt.figure("Oscillation: Euler fwd vs analytical")
-plt.title("Euler fwd vs analytical")
+plt.title("Euler fwd vs Matsuno with analytical")
 plt.xlabel("time\n(s)")
 plt.ylabel("u\n(m/s)")
 plt.plot(timeArr, uEulerArr, '.b', label="Euler")
+plt.plot(timeArr, uMatsunoArr, ".y", label="Matsuno")
 plt.plot(timeArr, uAnaArr, "r", label="Analytical")
-# !!! put the other methods here too
 plt.legend()
 
 ##plotting the other schemes
 plt.figure("Oscillation: others vs analytical")
-plt.title("Cd vs Heun vs Matsuno vs analytical")
+plt.title("Cd vs Heun with analytical")
 plt.xlabel("time\n(s)")
 plt.ylabel("u\n(m/s)")
 plt.plot(timeArr, uLeapFrogArr, ".c", label="Leapfrog")
 plt.plot(timeArr, uHeunArr, ".m", label="Heun")
-#plt.plot(timeArr, uMatsunoArr, ".y", label="Matsuno")
 plt.plot(timeArr, uAnaArr, "k", label="Analytical")
 plt.legend()
 
-##plot heun scheme
-
-
-##plot matsuno scheme
-
-#plotting the error (difference between the model and analytical solution)
-plt.figure("Error comparison")
-plt.title("Euler error")
+##energy plots
+plt.figure("Oscillations: energy")
+plt.title("Euler fwd vs cd vs Heun vs Matsuno energy conservation")
 plt.xlabel("time\n(s)")
-plt.ylabel("error (m/s)")
-plt.plot(timeArr, eulerErrArr, ".r", label="Euler")
+plt.ylabel("Energy")
+plt.semilogy(timeArr, eulerEngArr, label="Euler")
+plt.semilogy(timeArr, leapFrogEngArr, label="Leapfrog")
+plt.semilogy(timeArr, heunEngArr, label="Heun")
+plt.semilogy(timeArr, matsunoEngArr, label="Matsuno")
 plt.legend()
 
-plt.figure("other errors")
-plt.title("Cd vs Heun vs Matsuno error")
+##plotting the error (difference between the model and analytical solution)
+plt.figure("Error comparison")
+plt.title("Euler and Matsuno error")
+plt.xlabel("time\n(s)")
+plt.ylabel("error (m/s)")
+plt.semilogy(timeArr, eulerErrArr, ".r", label="Euler")
+plt.semilogy(timeArr, matsunoErrArr, ".b", label="Matsuno")
+plt.legend()
+
+plt.figure("Other errors")
+plt.title("Cd vs Heun error")
 plt.xlabel("time\n(s)")
 plt.ylabel("error (m/s)")
 plt.plot(timeArr, leapFrogErrArr, ".g", label="leapfrog")
 plt.plot(timeArr, heunErrArr, ".r", label="Heun")
-#plt.plot(timeArr, matsunoErrArr, ".b", label="Matsuno")
 plt.legend()
+
 
 
 ##Don't forget to show 'em if you got 'em
